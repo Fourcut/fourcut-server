ecf1594197215fcb616937b463dae7131bd01fbb
keepgoing-hyeonsoo <chs98105@gmail.com>
2022-11-20 03:38:58 +0900

    [feat]: cors 임시설정

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .db import database, models
 
 models.Base.metadata.create_all(database.engine)
 
 app = FastAPI()  # instance
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
