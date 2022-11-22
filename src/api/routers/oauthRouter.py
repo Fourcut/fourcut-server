dd915ea70d06bf68e82003512386f62f6649c1ce
keepgoing-hyeonsoo <chs98105@gmail.com>
2022-11-23 06:10:47 +0900

    [fix]: 카카오톡 로그인 로직 수정 (백엔드는 토큰생성위주)

--- src/api/routers/oauthRouter.py
+++ src/api/routers/oauthRouter.py
@@ -3,7 +3,7 @@ import os
 
 import requests
 from dotenv import load_dotenv
-from fastapi import APIRouter, Depends, Request, Response, status
+from fastapi import APIRouter, Body, Depends, Request, Response, status
 from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 
@@ -19,28 +19,15 @@ KAKAO_RESTAPI_KEY = os.environ.get("KAKAO_RESTAPI_KEY")
 REDIRECT_URI = os.environ.get("REDIRECT_URI")
 
 
-@router.get("/kakao")
-def kakaoClient():
-    url = f"https://kauth.kakao.com/oauth/authorize?client_id={KAKAO_RESTAPI_KEY}&response_type=code&redirect_uri={REDIRECT_URI}"
-
-    response = RedirectResponse(url)
-    return response
-
-
-@router.get("/kakao/redirect")
+@router.post("/kakao")
 async def kakaoAuth(
     response: Response,
-    code: str | None = "",
+    kakao_access_token: dict = Body(),
     db: Session = Depends(get_db),
 ):
     try:
-        _url = f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={KAKAO_RESTAPI_KEY}&code={code}&redirect_uri={REDIRECT_URI}"
-        _res = requests.post(_url)  # kakao token
-        _result = _res.json()  # kakao token
-        response.set_cookie(key="kakao", value=str(_result["access_token"]))
-
         get_user_url = "https://kapi.kakao.com/v2/user/me"
-        headers = {"Authorization": "Bearer " + _result["access_token"]}
+        headers = {"Authorization": "Bearer " + kakao_access_token["access_token"]}
         userdata = requests.post(url=get_user_url, headers=headers)
 
         _userdata = userdata.json()
@@ -53,14 +40,3 @@ async def kakaoAuth(
     except:
         response.status_code = status.HTTP_401_UNAUTHORIZED
         return {"error": "인증에 실패했습니다"}
-
-
-@router.get("/kakao/logout")
-def kakaoLogout(request: Request, response: Response):
-    url = "https://kapi.kakao.com/v1/user/unlink"
-    KEY = request.cookies["kakao"]
-    headers = dict(Authorization=f"Bearer {KEY}")
-    _res = requests.post(url, headers=headers)
-    response.set_cookie(key="kakao", value=None)
-    response.status_code = status.HTTP_200_OK
-    return {"logout": _res.json()}
