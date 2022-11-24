62feae44dfdbd2fd8f04416cd96ddbc07e94d75f
keepgoing-hyeonsoo <chs98105@gmail.com>
2022-11-25 01:45:17 +0900

    wip

--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ origins = ["http://localhost:3000", "http://ne-mo.me"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
